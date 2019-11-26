879bf53f662f289fe5f8ace1a1071144fd8ea565
Julie Norcross <julesnorc@gmail.com>
2019-11-26 11:43:27 -0500

    gh pages

---
+++ build/precache-manifest.085ec2760527a9808615f331682014e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "7ec4849d2fa3e77c2351",
+    "url": "/lights-out/static/js/main.7ec4849d.chunk.js"
+  },
+  {
+    "revision": "03109274422a2c9f0101",
+    "url": "/lights-out/static/js/1.03109274.chunk.js"
+  },
+  {
+    "revision": "7ec4849d2fa3e77c2351",
+    "url": "/lights-out/static/css/main.2446f616.chunk.css"
+  },
+  {
+    "revision": "7766ea75baf84ed601bc3502deb7beb6",
+    "url": "/lights-out/index.html"
+  }
+];
